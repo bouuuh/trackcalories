3449549729fda7904c4e8393067eee89c477f062
Candice <candice.doise@gmail.com>
2022-11-08 11:55:31 +0100

    second

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
     <title>Track Calories</title>
 </head>
 <body>
-    
+    <form action="connexion.php" method="post">
+        <input type="text" name="surname" placeholder="Entrez votre prénom">
+        <input type="text" name="name" placeholder="Entrez votre nom">
+        <input type="mail" name="mail" placeholder="Entrez votre adresse mail">
+        <input type="password" name="password" placeholder="Entrez votre mot de passe">
+        <input type="password" name="passwordconfirm" placeholder="Confirmez votre mot de passe">
+        <button type="submit">Suivant</button>
+    </form>
 </body>
 </html>
